552c3a83a998049dc8d92cc3c4a844cf4c37be17
jainhrithik <hrithikjain042@gmail.com>
2019-07-20 11:28:11 +0530

    added the getter and setter

--- Book.java
+++ Book.java
@@ -2,5 +2,21 @@ public class Book{
 
 public static void main(String are[]){
 System.out.println("hi");
+
+System.out.println("hjkgi");
+System.out.println("hisdfg");
+System.out.println("hsdfi");
+System.out.println("afghaai");
+System.out.println("gfhi");
+System.out.println("hafgri");
+
+public int add()
+{
+	int x=4+3;
+	return x	
+}
+
+int z=add();
+
 }
 }
